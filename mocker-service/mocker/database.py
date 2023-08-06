d2f1b735d2f5d05ff48a5342325a206a154d4c95
Grzegorz Z <59372707+gregzajac@users.noreply.github.com>
2023-08-06 21:59:29 +0200

    Added POST endpoint with test and small refactoring. (#17)
    * Added POST endpoint with test and small refactoring.
    
    * Deleted temporary 'tmp.py'.

---
+++ mocker-service/mocker/database.py
@@ -0,0 +1,92 @@
+"""Database module."""
+
+import logging
+from contextlib import contextmanager
+from datetime import datetime
+
+
+logger = logging.getLogger(__name__)
+
+
+class DbStorage(dict):
+    """A Singleton dictionary representing a key-value database.
+
+    The structure of the Endpoint:
+    - key (str): Endpoint URL,
+    - value (dict): Endpoint data, with the structure:
+        - "content" (str): Data provided for the Endpoint,
+        - "content_type" (str): Content-Type provided in the Response header,
+        - "endpoint_type" (str): Allowed Endpoint type (XML/JSON),
+        - "created" (datetime): Date and time of the endpoint creation.
+    """
+
+    def __new__(cls):
+        if not hasattr(cls, "instance"):
+            cls.instance = super(DbStorage, cls).__new__(cls)
+        return cls.instance
+
+
+db_example_xml = {
+    "sub-url/file.xml": {
+        "content": """<menu id="file" value="File">
+    <popup>
+        <menuitem value="New" onclick="CreateNewDoc()" />
+        <menuitem value="Open" onclick="OpenDoc()" />
+        <menuitem value="Close" onclick="CloseDoc()" />
+    </popup>
+</menu>""",
+        "content_type": "application/xml",
+        "endpoint_type": "XML",
+        "created": datetime(2023, 6, 1, 10, 15, 45),
+    },
+}
+
+db_example_md5 = {
+    "sub-url/file.md5": {
+        "content": "1e651ba4c5bcb60c6f796013d920a335",
+        "content_type": "text/html",
+        "endpoint_type": "XML",
+        "created": datetime(2023, 6, 1, 10, 15, 45),
+    },
+}
+
+db_example_json = {
+    "sub-url/file.json": {
+        "content": """{"menu": {
+    "id": "file",
+    "value": "File",
+    "popup": {
+        "menuitem": [
+            {"value": "New", "onclick": "CreateNewDoc()"},
+            {"value": "Open", "onclick": "OpenDoc()"},
+            {"value": "Close", "onclick": "CloseDoc()"}
+        ]
+    }
+}}""",
+        "content_type": "application/json",
+        "endpoint_type": "JSON",
+        "created": datetime(2023, 7, 2, 11, 25, 55),
+    },
+}
+
+
+def add_data_to_db(db: DbStorage, db_data: dict[str, dict]) -> None:
+    for key, value in db_data.items():
+        db[key] = value
+
+
+def remove_data_from_db(db: DbStorage) -> None:
+    db.clear()
+
+
+db_storage = DbStorage()
+# add_data_to_db(db_storage, db_example_xml | db_example_md5 | db_example_json)
+
+
+@contextmanager
+def session() -> dict[str, dict]:
+    try:
+        yield db_storage
+    except Exception:
+        logger.exception("Not connected to the database")
+        raise
--- mocker-service/mocker/main.py
+++ mocker-service/mocker/main.py
@@ -1,22 +1,44 @@
 """Main module."""
 
-from fastapi import FastAPI, Response, status
+import hashlib
+import logging
+from typing import Annotated
+
+from fastapi import Body, FastAPI, HTTPException, Response, status
 
 from mocker.config import settings
-from mocker.repositories import EndpointNotFoundError, endpoint_repository
+from mocker.database import session
+from mocker.repositories import (
+    EndpointDuplicatedError,
+    EndpointNotFoundError,
+    EndpointRepository,
+)
+
+
+logger = logging.getLogger(__name__)
 
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+endpoint_repository = EndpointRepository(session)
 
 
-@app.get("/", tags=["Test HTTP endpoint"])
-def home():
+@app.get("/", tags=["Get root"])
+def get_root():
     return "Placeholder!"
 
 
 @app.get(
-    "/api/{url:path}",
-    tags=["Get the endpoint by the given URL"],
+    "/endpoints/",
+    tags=["Get all Endpoints"],
+)
+def get_endpoints():
+    endpoints = endpoint_repository.get_all()
+    return endpoints
+
+
+@app.get(
+    "/endpoints/{url:path}",
+    tags=["Get Endpoint by URL"],
 )
 def get_endpoint(url: str):
     try:
@@ -24,5 +46,99 @@ def get_endpoint(url: str):
         return Response(
             content=url_data["content"], media_type=url_data["content_type"]
         )
-    except EndpointNotFoundError:
-        return Response(status_code=status.HTTP_404_NOT_FOUND)
+    except EndpointNotFoundError as err:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(err))
+
+
+@app.post(
+    "/endpoints/",
+    tags=["Create Endpoint"],
+)
+def add_endpoint(
+    url: Annotated[str, Body()],
+    content: Annotated[str, Body()],
+    endpoint_type: Annotated[str, Body()],
+):
+    """Add new endpoint (for XML add additional MD5 endpoint).
+
+    Args:
+        url (str): URL of the HTTP endpoint
+        content (str): data provided for the HTTP endpoint
+        endpoint_type (str): xml or json
+
+    Returns:
+        list[str]: A list of created URLs of the HTTP endpoints (one for JSON,
+        two for XML)
+    """
+    if endpoint_type.upper() == "XML":
+        # Add the XML Endpoint
+        try:
+            endpoint_repository.add(url, content, "application/xml", endpoint_type)
+
+            # Add the MD5 Endpoint for the previously added XML Endpoint
+            url_md5 = _create_url_with_md5_extension(url)
+            url_md5_content = hashlib.md5(content.encode()).hexdigest()
+            try:
+                endpoint_repository.add(
+                    url_md5, url_md5_content, "text/html", endpoint_type
+                )
+            except EndpointDuplicatedError:
+                # Remove a previously added XML Endpoint due to not added MD5 Endpoint
+                logger.critical(
+                    f"Cannot add MD5 Endpoint `{url_md5}` "
+                    f"even if corresponding XML Endpoint `{url}` has been added"
+                )
+                try:
+                    endpoint_repository.remove(url)
+                except EndpointNotFoundError:
+                    logger.critical(
+                        f"Cannot remove previously added XML endpoint `{url}`"
+                    )
+
+            return Response(
+                content=f"Utworzone endpointy:\n{[url, url_md5]}",
+                status_code=status.HTTP_201_CREATED,
+                media_type="text/html",
+            )
+
+        except EndpointDuplicatedError as err:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(err))
+
+    if endpoint_type.upper() == "JSON":
+        try:
+            endpoint_repository.add(url, content, "application/json", endpoint_type)
+            return Response(
+                content=f"Utworzone endpointy:\n{[url]}",
+                status_code=status.HTTP_201_CREATED,
+                media_type="text/html",
+            )
+        except EndpointDuplicatedError as err:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(err))
+
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail=f"Not allowed endpoint type `{endpoint_type}`",
+    )
+
+
+def _create_url_with_md5_extension(url: str) -> str:
+    """From given URL, create the one with the `.md5` extension.
+
+    Args:
+        url (str): Endpoint URL (can have only one dot as a file extension)
+
+    Returns:
+        str: Endpoint URL with changed extension to `.md5` (or added if file name
+            in the URL Endpoint didn't have it)
+    """
+    url_sections = url.split("/")
+    last_section = url_sections[-1]
+    last_section_dot_parts = last_section.split(".")
+
+    if len(last_section_dot_parts) == 2:
+        last_section = last_section_dot_parts[0] + ".md5"
+    else:
+        last_section = last_section + ".md5"
+    url_sections[-1] = last_section
+
+    return "/".join(url_sections)
